d57a5099599b2a5fd62f4d74116926c0c7aad8c4
User 14 <User 14@PC15>
2021-12-08 11:06:00 +0200

    Add project files.

---
+++ StringLength/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Ivan";
+            int c1 = firstName.Length; //4
+            string lastName = "Petrov";
+            int c2 = lastName.Length; //6
+            string fullName = string.Concat(firstName, " ", lastName); //Ivan Petrov
+            int c3 = fullName.Length; //11
+
+        }
+    }
+}
